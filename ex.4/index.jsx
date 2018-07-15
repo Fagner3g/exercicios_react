6c8fb167c097574febf8831665cb4a91fa67af2c
Fagneregomes <fagner.egomes@gmail.com>
2018-07-15 13:54:25 -0300

    Aula 24 - Componente dentro de componente.

--- ex.4/index.jsx
+++ ex.4/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 // import {Primeiro, Segundo} from './component'
-import SilvaFamily, { Segundo} from './silvaFamily'
+import Primeiro, { Segundo} from './component'
 
 // Todo componente em react pode ser uma function ou uma class
 
 ReactDom.render(
     <div>
-        <SilvaFamily/>
+        <Primeiro />
+        <Segundo />
     </div>,
     document.getElementById('root')
 )
---
+++ ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default () => (
+    <div>
+        <Member name="Fagner" lastName="Silva" />
+        <Member name="Lucas" lastName="Silva" />
+        <Member name="Felipe" lastName="Silva" />
+        <Member name="Joao" lastName="Silva" />
+    </div>
+)
